46240b8f79924b3b096ef2e3586e1088424b6062
Pratyush <pratyush.creed@gmail.com>
2016-02-19 13:31:10 +0530

    Circular spiral drawer
    A python script to take over your mouse control to make a spiral design for you.
    You can change attributes of spiral easily.

---
+++ circular_spiral_drawer/draw.py
@@ -0,0 +1,48 @@
+# Python 3 circular spiral script
+
+import pyautogui, time, math
+
+# change the parameters below to generate awesome arts :)
+radius = 200 # radius for spiral
+quadrant = 1 # 1, 2, 3, 4; 4 by default;
+min_radius = 10 # last size of radius; radius decreases in spiral;
+decreasing_factor = 0.995 # radius decreasing factor; higher means more circular spiral;
+angle_increment = 3 # less means more detailed thus slow;
+pyautogui.PAUSE = 0.01 # determines speed of task
+
+def location(quadrant, radius, posX, posY):
+    radian = math.radians(angle)
+    if quadrant == 1:
+        locX = math.sin(radian) * radius +  posX
+        locY = math.cos(radian) * radius + posY
+    elif quadrant == 2:
+        locX = math.sin(radian) * -radius +  posX
+        locY = math.cos(radian) * radius + posY
+    elif quadrant == 2:
+        locX = math.sin(radian) * -radius +  posX
+        locY = math.cos(radian) * -radius + posY
+    else:
+        locX = math.sin(radian) * radius +  posX
+        locY = math.cos(radian) * -radius + posY
+    list_location = []
+    list_location.append(locX)
+    list_location.append(locY)
+    return tuple(list_location)
+
+time.sleep(1.5)
+posX, posY = pyautogui.position()
+pyautogui.click()
+angle = 0
+
+
+pyautogui.moveTo(location(quadrant, radius, posX, posY))
+pyautogui.mouseDown(button='left')
+
+while radius > min_radius:
+    pyautogui.moveTo(location(quadrant, radius, posX, posY))
+    angle += angle_increment
+    radius *= decreasing_factor
+    if angle > 359:
+        angle = 0
+
+pyautogui.mouseUp(button='left')
